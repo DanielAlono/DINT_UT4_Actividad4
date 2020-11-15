c8259b7e7a374dd90f45b44974edf04565367092
Daniel Alonso <de.alonso@iesdoctorbalmis.com>
2020-11-15 21:23:39 +0100

    Agregar archivos de proyecto.

---
+++ UT4_Actividad4/BordeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UT4_Actividad4
+{
+    public class BordeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if((bool)value == true)
+                {
+                    return 3;
+                }
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if((bool)value == false)
+                {
+                    return 0;
+                }
+            }
+            return 3;
+        }
+    }
+}
